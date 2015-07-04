250213af30a64b5b004ac305d44b896a260a1f80
thomaslam <tuananhlam95@gmail.com>
2015-07-03 19:12:31 -0700

    payment setting added

--- client/app/components/payment/paymentController.js
+++ client/app/components/payment/paymentController.js
@@ -2,10 +2,25 @@
 
 //load module
 angular.module('trApp')
-  .controller('paymentController', ['$scope', '$location', inboxController]);
+  .controller('paymentController', ['$scope', 'PaymentService', paymentController]);
 
-  function paymentController() {
+  function paymentController($scope, PaymentService) {
+    $scope.card = {};
+    // $scope.user = {};
 
+    PaymentService.check();
+    // .then(function(response) {
+    //   $scope.user = response.data;
+    // })
+
+    $scope.save = function() {
+      console.log($scope.card);
+      PaymentService.update($scope.card).then(function() {
+        $scope.statusMessage = 'Card info updated successfully';
+      }).catch(function() {
+        $scope.statusMessage = 'Error updating card info!';
+      });
+    }
   }
 
 })();
